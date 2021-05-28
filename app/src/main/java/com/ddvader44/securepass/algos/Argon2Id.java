2fa55e71aa791df2d4dfed5beadabc6a9c42e449
Dishit <40226856+ddvader44@users.noreply.github.com>
2021-05-29 00:24:02 +0530

    updated Argon2Id algo

--- app/src/main/java/com/ddvader44/securepass/algos/Argon2Id.java
+++ app/src/main/java/com/ddvader44/securepass/algos/Argon2Id.java
@@ -1,15 +1,31 @@
 package com.ddvader44.securepass.algos;
 
-import de.mkammerer.argon2.Argon2;
-import de.mkammerer.argon2.Argon2Factory;
+import org.signal.argon2.Argon2;
+import org.signal.argon2.Argon2Exception;
+import org.signal.argon2.MemoryCost;
+import org.signal.argon2.Type;
+import org.signal.argon2.Version;
+
 
 public class Argon2Id {
 
-     public static String argon(String message){
+     public static String argon(String message) throws Argon2Exception {
 
-        Argon2 argon2 = Argon2Factory.create();
-        char[] password = message.toCharArray();
-        return argon2.hash(10, 65536, 1, password);
+         Argon2 argon2 = new Argon2.Builder(Version.V13)
+                 .type(Type.Argon2id)
+                 .memoryCost(MemoryCost.MiB(32))
+                 .parallelism(1)
+                 .iterations(3)
+                 .build();
+
+         String salt = "YOtX2//7NoD/owm8RZ8llw==";
+
+         Argon2.Result result = argon2.hash(message.getBytes(), salt.getBytes());
+
+         //byte[] hash    = result.getHash();
+         //String encoded = result.getEncoded();
+
+         return result.getHashHex();
 
     }
 
